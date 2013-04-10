05b57b01eb638cdc6165dc5f2ec7b18d8b0446e9
Michael Akerman <mike@uark.edu>
2013-04-10 16:08:39 -0500

    Initial add src

---
+++ src/main/java/ResponseHeaderFilter.java
@@ -0,0 +1,50 @@
+package edu.uark.util.servletfilters;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+/**
+ * @author Steven Cole
+ */
+public final class ResponseHeaderFilter implements Filter
+{
+	private FilterConfig fc = null;
+
+	public void init ( final FilterConfig fc ) throws ServletException
+	{
+		this.fc = fc;
+	}
+
+	@Override
+	public void doFilter ( final ServletRequest request, final ServletResponse response, final FilterChain chain ) throws IOException, ServletException
+	{
+		doFilter( (HttpServletRequest) request, (HttpServletResponse) response, chain );
+	}
+
+	private void doFilter ( final HttpServletRequest request, final HttpServletResponse response, final FilterChain chain ) throws IOException, ServletException
+	{
+		final Enumeration parameters = fc.getInitParameterNames();
+		while ( parameters.hasMoreElements() )
+		{
+			final String parameter = (String)parameters.nextElement();
+			if ( response.containsHeader( parameter ) )
+			{
+				response.setHeader( parameter, fc.getInitParameter( parameter ) );
+			}
+			else
+			{
+				response.addHeader( parameter, fc.getInitParameter( parameter ) );
+			}
+		}
+		chain.doFilter( request, response );
+	}
+
+
+	public void destroy ()
+	{
+		//required by the Filter interface
+	}
+}
